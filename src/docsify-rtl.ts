7d03382867fd9df2e32870efa156b79c84dcb8d3
KoLiBer <koliberr136a1@gmail.com>
2019-08-06 15:32:17 +0430

    feat: add docsify rtl source

--- src/docsify-rtl.ts
+++ src/docsify-rtl.ts
@@ -1,3 +1,25 @@
-export default () => {
-    console.log("A");
+export interface RTLConfig {
+    body?: "rtl" | "ltr";
+    bdo?: "rtl" | "ltr";
+}
+
+export const install = (hook: any, vm: any) => {
+    const config: RTLConfig = Object.assign(
+        {},
+        {
+            tag: "rtl"
+        },
+        vm.config.rtl
+    );
+
+    hook.afterEach(function(html: any, next: any) {
+        document.getElementsByName("body").forEach(item => {
+            item.dir = config.body || "rtl";
+        });
+        document.getElementsByName("bdo").forEach(item => {
+            item.dir = config.bdo || "rtl";
+        });
+
+        next(html);
+    });
 };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+import { install } from "./docsify-rtl";
+
+if (!(window as any).$docsify) {
+    (window as any).$docsify = {};
+}
+
+(window as any).$docsify.plugins = (
+    (window as any).$docsify.plugins || []
+).concat(install);
+
 export * from "./docsify-rtl";
