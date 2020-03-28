dedca215ed55d5a245227637b6d087b706fd448b
KoLiBer <koliberr136a1@gmail.com>
2020-03-28 13:39:46 +0430

    build: rebuild

--- build/docsify-rtl.js
+++ build/docsify-rtl.js
@@ -19,6 +19,14 @@ exports.install = function (hook, vm) {
             var item = _e[_d];
             item.dir = config.bdo;
         }
+        for (var _f = 0, _g = document.getElementsByTagName("pre"); _f < _g.length; _f++) {
+            var item = _g[_f];
+            item.dir = "ltr";
+        }
+        for (var _h = 0, _j = document.getElementsByTagName("code"); _h < _j.length; _h++) {
+            var item = _j[_h];
+            item.dir = "ltr";
+        }
     });
 };
 //# sourceMappingURL=docsify-rtl.js.map
